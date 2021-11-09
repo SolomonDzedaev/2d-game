eee16ad8bfb9e5cf51668df4fe222afbcc217728
SolomonDzedaev <92586090+SolomonDzedaev@users.noreply.github.com>
2021-11-09 17:37:15 +0400

    Add files via upload
    Добавил койны и хп 
    и когда вы потеряете хп то вам предложат начать заново

--- script.js
+++ script.js
@@ -27,10 +27,28 @@ arrPicPlayer['left'] = picPlayerLeft;
 arrPicPlayer['right'] = picPlayerRight;
 let arrPicEnemy = [];
 arrPicEnemy['left'] = picEnemyLeft;
-arrPicEnemy['right'] = picEnemyRight
+arrPicEnemy['right'] = picEnemyRight;
+
+
+let picLife = new Image();
+picLife.src = "./pic/Life.png";
+
+let picCoin = new Image();
+picCoin.src = "./pic/Coin.png";
 
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
+
+function generateRandomPosition(pic,xMin, xMax,yMin,yMax){
+    let x =Math.random()*(xMax-xMin)+xMin,
+    y=Math.random()*((yMax-pic.width)+yMin);
+    return [x,y];
+}
+
+let PositionCoin=[0,0]
+function newPositionCoin(){
+    PositionCoin=generateRandomPosition(picCoin,0,innerWidth,innerHeight*0.5,innerHeight*0.88);
+}
 
 function resizeImg(img, percent) {
     let prop;
@@ -49,11 +67,11 @@ let startGame = false;
 
 let xPlayer = 50,
 yPlayer = 665,
-    speedPlayer = 5, speedEnemy=50; navPlayer = 'right';
+    speedPlayer = 5, speedEnemy=50; navPlayer = 'right',
     navEnemy="left",
-    xEnemy=660;
-    yEnemy=665;
-
+    xEnemy=660,
+    yEnemy=665, boardPicPlayer=0, boardPicEnemy=0;
+countLife = 1;
 
     function boardPic(pic, x, y){
       let picRight, picBottom;
@@ -67,9 +85,11 @@ yPlayer = 665,
         let picEnemy = arrPicEnemy[navEnemy]
         resizeImg(picPlayer, 8);
         resizeImg(picEnemy, 8);
-        let boardPicPlayer = boardPic(picPlayer, xPlayer, yPlayer)
+        resizeImg(picLife, 5);
+        resizeImg(picCoin, 5);
+         boardPicPlayer = boardPic(picPlayer, xPlayer, yPlayer)
          
-        let boardPicEnemy = boardPic(picEnemy, xEnemy, yEnemy)
+         boardPicEnemy = boardPic(picEnemy, xEnemy, yEnemy);
         
 
         function startPosition(){
@@ -79,7 +99,29 @@ yPlayer = 665,
             xEnemy = window.innerWidth*0.85;
             yEnemy = window.innerHeight*0.88-picEnemy.height
         }
+           
+            
+
         function moveEnemy(){
+            function collisionEnemy(){
+                if(boardPicPlayer[0]>xEnemy&&xPlayer < boardPicEnemy[0] &&yPlayer<boardPicEnemy[1]&&yEnemy<boardPicPlayer[1]) {
+                countLife--;
+                if(countLife<+0){
+                    let newGame = confirm('GamerOver:(/nХотите начать заново?');
+                    if(newGame){
+                        countLife=5;
+                    }
+                    else{
+                        clesrTimeout(timerMoveEnemy);
+                        startPosition();
+                        return;
+                    }
+                }
+                 
+                startPosition();
+                }
+            }
+            collisionEnemy();
             let timerMoveEnemy = setTimeout(()=>{
                 if(xEnemy>(xPlayer+picPlayer.width)){
                     xEnemy--;
@@ -96,11 +138,20 @@ yPlayer = 665,
         if(startGame){
             startPosition
             moveEnemy();
+            newPositionCoin();
             startGame=false;
         }
         ctx.drawImage(picBackground, 0, 0, window.innerWidth, window.innerHeight);
         ctx.drawImage(picPlayer, xPlayer, yPlayer, picPlayer.width, picPlayer.height);
         ctx.drawImage(picEnemy, xEnemy, yEnemy, picEnemy.width, picEnemy.height);
+        ctx.drawImage(picCoin, PositionCoin[0], PositionCoin[1], picCoin.width, picCoin.height);
+
+        for(let i=0; i<countLife;i++){
+            let yLife = innerHeight*0.1;
+            let xLife = innerWidth*0.05+picLife.width*1
+            ctx.drawImage(picLife, xLife, yLife, picLife.width, picLife.height);
+        }
+        
 
     }
 
